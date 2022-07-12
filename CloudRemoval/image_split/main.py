40ecc4f36c323c44b4ad1bdf081070b87af9d98c
yzz <1725438698@qq.com>
2022-07-12 10:13:11 +0800

    non-violent part finished

--- CloudRemoval/image_split/main.py
+++ CloudRemoval/image_split/main.py
@@ -24,28 +24,46 @@ if __name__ == '__main__':
         print("输入图片size过小")
         exit(0)
 
-    # 暴力划分
+    print("violent part")
+# 暴力图像划分
     out = image_processing.image_split_violent(img0, sub_image_size)
     print("size of origin image: %s" % str(img0.size))
     print("size of sub-image : %s" % str(out['images'][0][0].size))
     print("size of sub-image list: %s" % str((len(out['images']), len(out['images'][0]))))
-    count = 0
+    
     # 保存图像集
-    for i in out['images']:
-        count0 = 0
-        for j in i:
-            j.save(output_path + '/list/' + str(count) + '_' + str(count0) + '.jpg')
-            count0 += 1
-        count += 1
+    for i in range(len(out['images'])):
+        for j in range(len(out['images'][0])):
+            out['images'][i][j].save(output_path + '/list/' + str(i) + '_' + str(j) + '.jpg')
 
 
-    # 暴力合并
+# 暴力图像合并
     out1 = image_processing.image_merge_violent(out['images'], out['origin_size'])
     print("size of merge image: %s" % str(out1.size))
 
     # 保存图像
     out1.save(output_path + '/twitter.jpg')
 
+
+    print("\n\nnon-violent part")
+# 图像划分
+    out2 = image_processing.image_split(img0, sub_image_size)
+    print("size of origin image: %s" % str(img0.size))
+    print("size of sub-image : %s" % str(out2['images'][0][0].size))
+    print("size of sub-image list: %s" % str((len(out2['images']), len(out2['images'][0]))))
+    
+    # 保存图像集
+    for i in range(len(out2['images'])):
+        for j in range(len(out2['images'][0])):
+            out2['images'][i][j].save(output_path + '/list2/' + str(i) + '_' + str(j) + '.jpg')
+
+
+# 图像合并
+    out3 = image_processing.image_merge(out2['images'], out2['locations'], out2['origin_size'])
+    print("size of merge image: %s" % str(out3.size))
+
+    # 保存图像
+    out3.save(output_path + '/twitter2.jpg')
 
     
 
--- CloudRemoval/image_split/util/image_processing.py
+++ CloudRemoval/image_split/util/image_processing.py
@@ -52,11 +52,77 @@ def image_split_violent (img, size = 256):
     result = {'images': img_list, 'origin_size': origin_size}
     return result
 
-def image_split():
-    return 0
+
+# 确认最小重叠宽度
+# 划分图像
+def image_split(img, size):
+    """
+    划分图像
+
+    Parameters:
+        img - the origin image
+        size - the side length of sub-image, default 256
+
+    Returns:
+	    Return a dict, which include the split sub-images, their location and the size of origin image
+        example: {'images': [img0, img1, ...], 'locations': [(0,0), (0, 256), ...., (256, 256)]'size': [512, 512]}
+    """
+    origin_size = img.size  #原图大小
+    height = origin_size[1] #原图高
+    width = origin_size[0]  #原图宽
+
+    min_overlap = 75 # 两章子图重叠部分的最小宽度
+
+    #计算此时所需的行数和列数
+    row_num = math.ceil((height - min_overlap) / (size - min_overlap))
+    column_num = math.ceil((width - min_overlap) / (size - min_overlap))
+
+    dx = height / row_num
+    dy = width / column_num
+
+    
+    img_list = [[0 for i in range(column_num)] for i in range(row_num)]
+    location_list = [[0 for i in range(column_num)] for i in range(row_num)]
+    y = 0   #初始化y值
+    for i in range(row_num):
+        x = 0   #初始化x值
+        for j in range(column_num):
+            x0 = x
+            x1 = x + size
+            y0 = y
+            y1 = y + size
+            box = (round(x0), round(y0), round(x1), round(y1))
+            sub_img = img.crop(box)
+            
+            img_list[i][j] = sub_img
+            location_list[i][j] = (round(x0), round(y0))
+            #保证恰好取到最后一列像素
+            if(j == column_num - 1):
+                x = width - size
+            else:
+                x += dx
+        #保证恰好取到最后一行像素
+        if(i == row_num - 1):
+            y = height - size
+        else:
+            y += dy
+
+
+    result = {'images': img_list, "locations": location_list, 'origin_size': origin_size}
+    return result
 
 
 def image_merge_violent(img_list, origin_size):
+    """
+    暴力合并图像
+
+    Parameters:
+        img_list - the sub-image list
+        origin_size - the size of origin image
+
+    Returns:
+	    Return a image which merge all the sub-image
+    """
     size = img_list[0][0].size[0]
     img = Image.new('RGB', origin_size)
 
@@ -85,5 +151,22 @@ def image_merge_violent(img_list, origin_size):
     return img
 
 
-def image_merge(img_list):
-    return 0
+def image_merge(img_list, location_list, origin_size):
+    """
+    合并图像
+
+    Parameters:
+        img_list - the sub-image list
+        location_list - the locations of sub-images
+        origin_size - the size of origin image
+
+    Returns:
+	    Return a image which merge all the sub-image
+    """
+    img = Image.new('RGB', origin_size)
+
+    for i in range(len(img_list)):
+        for j in range(len(img_list[0])):
+            img.paste(img_list[i][j], location_list[i][j])
+
+    return img
